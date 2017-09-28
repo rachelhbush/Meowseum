f10ac2c32b1b8012007d2347df922b62e443360d
Rachel Bush <rachel.h.bush@gmail.com>
2017-09-28 07:31:19 -0500

    feat: Add night mode to context variables

--- Meowseum/context_processors.py
+++ Meowseum/context_processors.py
@@ -1,6 +1,24 @@
 # Description: This file is for setting custom sitewide template variables. These variables will be available in every template without first having to define them in a view.
+# The top-level functions are referenced by settings.py.
 
-def static_variables(request):
+# Define variables that will always be the same throughout the site.
+def constant_variables(request):
     # The app_name variable will save a lot of work if the developer decides to rename the app, because it appears in page titles, the footer, etc.
     # It can't be used for image attributes, however, because Django will interpret curly braces within quotation marks literally.
     return {'app_name':'Meowseum'}
+
+# 0. Define variables related to site settings.
+def settings_variables(request):
+    return {'night_mode':get_night_mode_status(request)}
+
+# 1. Detect whether night mode is on.
+# Input: request
+# Output: night_mode, a Boolean value for whether night mode is on
+def get_night_mode_status(request):
+    if 'night_mode' in request.session:
+        if request.session['night_mode']:
+            return True
+        else:
+            return False
+    else:
+        return True
--- Meowseum_Project/settings - production version.py
+++ Meowseum_Project/settings - production version.py
@@ -76,6 +76,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'Meowseum.context_processors.constant_variables',
+                'Meowseum.context_processors.settings_variables',
             ],
         },
     },
