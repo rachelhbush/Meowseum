8b62f8199045cd396f4e0b35feb3fb293b89cc00
Rachel Bush <rachel.h.bush@gmail.com>
2017-10-15 21:15:20 -0500

    style: Change variable name 'record' to 'upload' in view

--- Meowseum/views/upload_page1.py
+++ Meowseum/views/upload_page1.py
@@ -17,25 +17,25 @@ from Meowseum.file_handling.file_utility_functions import make_unique_with_rando
 def page(request):
     # Try to get the logged-in user's most recent file submission.
     try:
-        record = Upload.objects.filter(uploader=request.user).order_by('-id')[0]
+        upload = Upload.objects.filter(uploader=request.user).order_by('-id')[0]
     except IndexError:
         # If the user hasn't submitted a file yet, then the user shouldn't be here.
         raise PermissionDenied
     
     form = UploadPage1(request.POST or None, initial={'upload_type':'pets', 'tags':'#'}, request=request)
     if form.is_valid():
-        update_and_save_upload_record(form, record)
-        rename_upload_file(record, hosting_limits_for_Upload['poster_directory'])
+        update_and_save_upload_record(form, upload)
+        rename_upload_file(upload, hosting_limits_for_Upload['poster_directory'])
         # Users will automatically Like their own uploads. Use get_or_create because there is nothing stopping a user from
         # going back to this page again after the upload has been submitted and using it to edit the user's most recent upload, even though when
         # the editing feature is introduced, it will use a separate view.
-        Like.objects.get_or_create(upload=record, liker=request.user)
-        return redirect_to_next_page(form.cleaned_data['upload_type'], record.relative_url)
+        Like.objects.get_or_create(upload=upload, liker=request.user)
+        return redirect_to_next_page(form.cleaned_data['upload_type'], upload.relative_url)
     else:
         # Send the following variables to the template.
         context = {'form': form,
                    'upload': upload,
-                   'heading': 'Uploading ' + record.metadata.original_file_name + record.metadata.original_extension,
+                   'heading': 'Uploading ' + upload.metadata.original_file_name + upload.metadata.original_extension,
                    'upload_directory': Upload.UPLOAD_TO,
                    'poster_directory': hosting_limits_for_Upload['poster_directory'],
                    'has_contact_information': request.user.user_profile.has_contact_information(),
@@ -43,11 +43,11 @@ def page(request):
         return render(request, 'en/public/upload_page1.html', context)
 
 # 2. Update the upload record with the title, description, tags, and whether or not the upload is publicly listed, then save.
-def update_and_save_upload_record(form, record):
-    record.title = form.cleaned_data['title']
-    record.description = form.cleaned_data['description']
-    record.is_publicly_listed = form.cleaned_data['is_publicly_listed']
-    record.uploader_has_disabled_comments = form.cleaned_data['uploader_has_disabled_comments']
+def update_and_save_upload_record(form, upload):
+    upload.title = form.cleaned_data['title']
+    upload.description = form.cleaned_data['description']
+    upload.is_publicly_listed = form.cleaned_data['is_publicly_listed']
+    upload.uploader_has_disabled_comments = form.cleaned_data['uploader_has_disabled_comments']
 
     tags_from_title_and_description = get_tags_from_title_and_description(form.cleaned_data['title'], form.cleaned_data['description'])
     tags_from_tag_form = get_tags_from_tag_form(form.cleaned_data['tags'])
@@ -59,15 +59,15 @@ def update_and_save_upload_record(form, record):
         try:
             # If the tag does exist, then associate this upload record with it.
             existing_tag = Tag.objects.get(name=tag)
-            existing_tag.uploads.add(record)
+            existing_tag.uploads.add(upload)
             existing_tag.save()
         except Tag.DoesNotExist:
-            # If the tag doesn't exist, create a new one and add the most recent upload as the first record.
+            # If the tag doesn't exist, create a new one and add the most recent upload as its first record.
             new_tag = Tag(name=tag)
             new_tag.save()
-            new_tag.uploads.add(record)
+            new_tag.uploads.add(upload)
             new_tag.save()
-    record.save()
+    upload.save()
 
 # 2.1. Input: title and description strings. Output: A list of tags using the format ["blep", "catloaf"]. All tags are stored in lowercase form.
 def get_tags_from_title_and_description(title, description):
@@ -99,13 +99,13 @@ def get_tags_from_tag_form(string):
 
 # 3. Rename the upload's file using up to 182 of the first characters of its title. If the file name already exists, then add an underscore and 7-character random ID
 # to the end, replacing characters of the title if it is needed to stay within the limit. This function also accounts for the underscore-using, file name-based URL needing to be unique.
-def rename_upload_file(record, poster_directory):
+def rename_upload_file(upload, poster_directory):
     # Obtain all the strings that will be used for renaming.
-    upload_directory_name = os.path.split(record.file.name)[0]
+    upload_directory_name = os.path.split(upload.file.name)[0]
     upload_directory_path = os.path.join(settings.MEDIA_PATH, upload_directory_name)
-    old_file_name = record.metadata.file_name
-    extension = record.metadata.extension
-    new_file_name = get_new_file_name(record)
+    old_file_name = upload.metadata.file_name
+    extension = upload.metadata.extension
+    new_file_name = get_new_file_name(upload)
     old_full_file_name = old_file_name + extension
     new_full_file_name = new_file_name + extension
     
@@ -114,19 +114,19 @@ def rename_upload_file(record, poster_directory):
     new_absolute_path = os.path.join(upload_directory_path, new_full_file_name)
     os.rename(old_absolute_path, new_absolute_path)
     # If the file is for a <video>, then rename its poster.
-    if record.metadata.mime_type.startswith('video'):
+    if upload.metadata.mime_type.startswith('video'):
         old_poster_path = os.path.join(settings.MEDIA_PATH, Upload.UPLOAD_TO, poster_directory, old_file_name + '.jpg')
         new_poster_path = os.path.join(settings.MEDIA_PATH, Upload.UPLOAD_TO, poster_directory, new_file_name + '.jpg')
         os.rename(old_poster_path, new_poster_path)
         
     # If a thumbnail exists in the OS, then rename the thumbnail.
-    if record.metadata.width > 600:
+    if .metadata.width > 600:
         thumbnail_directory = hosting_limits_for_Upload['thumbnail'][2]
         old_thumbnail_path = os.path.join(settings.MEDIA_PATH, Upload.UPLOAD_TO, thumbnail_directory, old_full_file_name)
         new_thumbnail_path = os.path.join(settings.MEDIA_PATH, Upload.UPLOAD_TO, thumbnail_directory, new_full_file_name)
         os.rename(old_thumbnail_path, new_thumbnail_path)
         # If the file is for a <video>, then rename its thumbnail's poster.
-        if record.metadata.mime_type.startswith('video'):
+        if upload.metadata.mime_type.startswith('video'):
             old_poster_thumbnail_path = os.path.join(settings.MEDIA_PATH, Upload.UPLOAD_TO, poster_directory, thumbnail_directory, old_file_name + '.jpg')
             new_poster_thumbnail_path = os.path.join(settings.MEDIA_PATH, Upload.UPLOAD_TO, poster_directory, thumbnail_directory, new_file_name + '.jpg')
             os.rename(old_poster_thumbnail_path, new_poster_thumbnail_path)
@@ -137,15 +137,15 @@ def rename_upload_file(record, poster_directory):
 
     # Rename the file in Django's database.
     new_relative_path = os.path.join(upload_directory_name, new_file_name + extension).replace('\\','/') # This is the part of the path after /media/.
-    record.file.name = new_relative_path
-    record.relative_url = new_file_name.replace(" ","_")
-    record.save()
-    record.metadata.file_name = new_file_name
-    record.metadata.save()
+    upload.file.name = new_relative_path
+    upload.relative_url = new_file_name.replace(" ","_")
+    upload.save()
+    upload.metadata.file_name = new_file_name
+    upload.metadata.save()
 
 # 3.1 Return the new name for the file.
-def get_new_file_name(record):
-    hypothetical_file_name = record.title
+def get_new_file_name(upload):
+    hypothetical_file_name = upload.title
     # Remove characters that are unsupported in a common operating system or may lead to security vulnerabilities.
     hypothetical_file_name = get_valid_file_name(hypothetical_file_name)
     if hypothetical_file_name == '_':
@@ -153,7 +153,7 @@ def get_new_file_name(record):
         # make_unique_with_random_id_suffix_within_character_limit() returns a random ID if passed an empty string. 
         # Reset the underscore to an empty string, with the effect of disallowing '_' as a file name.
         hypothetical_file_name = ''
-    return make_unique_with_random_id_suffix_within_character_limit(hypothetical_file_name, 178, file_name_and_url_will_be_unique, record)
+    return make_unique_with_random_id_suffix_within_character_limit(hypothetical_file_name, 178, file_name_and_url_will_be_unique, upload)
 
 # 4. After successfully processing the form, redirect to the homepage or the next page of the form if there is one.
 # Input: upload_type, a string for the category. relative_url, a string which will be used when the upload is in the Pets category.
