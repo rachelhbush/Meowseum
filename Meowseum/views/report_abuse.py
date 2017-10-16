aec2c52291d091a959f9140880ece6d867ea2ba4
Rachel Bush <rachel.h.bush@gmail.com>
2017-10-16 00:44:57 -0500

    refactor: Refactor and refine report_abuse.py

--- Meowseum/views/report_abuse.py
+++ Meowseum/views/report_abuse.py
@@ -6,36 +6,37 @@ from Meowseum.forms import AbuseReportForm
 from django.shortcuts import render
 from Meowseum.common_view_functions import redirect, increment_hit_count
 
+# 0. Main function
 def page(request):
-    # Automatically fill in two of the fields based on the page from which the user is visiting.
-    initial_data = {}
-    try:
-        initial_data['offending_username'] = request.GET['offending_username']
-    except:
-        initial_data['offending_username'] = ''
-    try:
-        initial_data['url'] = 'http://www.meowseum.com' + request.GET['referral_url']
-    except:
-        initial_data['url'] = ''
-    form = AbuseReportForm(request.POST or None, initial=initial_data)
-    
+    form = AbuseReportForm(request.POST or None, initial=get_initial_data_from_querystring(request))   
     if form.is_valid():
         offending_user = User.objects.get(username=form.cleaned_data['offending_username'])
+        new_record = AbuseReport(offending_user=offending_user,
+                                 abuse_type=form.cleaned_data['abuse_type'],
+                                 abuse_description=form.cleaned_data['abuse_description'],
+                                 url=form.cleaned_data['url'])
         if request.user.is_authenticated():
-            filer = request.user
-            new_record = AbuseReport(filer=filer,
-                                     offending_user=offending_user,
-                                     abuse_type=form.cleaned_data['abuse_type'],
-                                     abuse_description=form.cleaned_data['abuse_description'],
-                                     url=form.cleaned_data['url'])
-        else:
-            new_record = AbuseReport(offending_user=offending_user,
-                                     abuse_type=form.cleaned_data['abuse_type'],
-                                     abuse_description=form.cleaned_data['abuse_description'],
-                                     url=form.cleaned_data['url'])
+            new_record.filer = request.user
         new_record.save()
         return redirect('index')
     else:
         if request.method == 'GET':
             increment_hit_count(request, "report_abuse")
         return render(request, 'en/public/report_abuse.html', {'form': form})
+
+# 1. Automatically fill in two of the fields based on the page from which the user is visiting.
+# Input: request.
+# Output: initial_data, a dictionary.
+def get_initial_data_from_querystring(request):
+    initial_data = {}
+    try:
+        initial_data['offending_username'] = request.GET['offending_username']
+    except KeyError:
+        initial_data['offending_username'] = ''
+    try:
+        # The referral URL is relative to the website domain name, but the URL validator will only validate a full URL.
+        # So, prefix a site domain name even though the domain name is liable to change in the future.
+        initial_data['url'] = 'http://www.meowseum.com' + request.GET['referral_url']
+    except KeyError:
+        initial_data['url'] = ''
+    return initial_data
