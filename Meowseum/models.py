0955a5a132c40cfa7383b1f24703e991e6abd4bf
Rachel Bush <rachel.h.bush@gmail.com>
2017-11-01 06:34:24 -0500

    refactor: User contact info form now uses label variables
    In the template for the user contact information form, I replaced the
    <label>s with Django's template variables for labels. The template now
    gets the information from the verbose_name of the model. I changed the
    labels "birthday" and "email" to "date of birth" and "email address" to
    be more formal and precise.

--- Meowseum/models.py
+++ Meowseum/models.py
@@ -377,12 +377,12 @@ class UserContact(models.Model):
     address_line_1 = models.CharField(max_length=255, verbose_name="address line 1", default="", blank=True)
     address_line_2 = models.CharField(max_length=255, verbose_name="address line 2", default="", blank=True)
     city = models.CharField(max_length=60, verbose_name="city", default="", blank=True)
-    state_or_province = models.CharField(max_length=60, verbose_name="state/Province", choices=(('', 'Select a state'),) + STATE_OR_PROVINCE_CHOICES, default="", blank=True)
+    state_or_province = models.CharField(max_length=60, verbose_name="State/Province", choices=(('', 'Select a state'),) + STATE_OR_PROVINCE_CHOICES, default="", blank=True)
     country = models.CharField(max_length=60, verbose_name="country", choices=(('', 'Select a country'),) + COUNTRY_CHOICES, default="", blank=True)
     zip_code = models.CharField(max_length=20, verbose_name="ZIP/Postal Code", default="", blank=True)
     phone_number = models.CharField(max_length=20, verbose_name="phone number", default="", blank=True)
     date_of_birth = models.DateField(verbose_name="date of birth", null=True, blank=True)
-    has_volunteering_interest = models.BooleanField(verbose_name="has an interest in volunteering", default=False, blank=True)
+    has_volunteering_interest = models.BooleanField(verbose_name="I am interested in volunteering with an animal shelter or rescue group.", default=False, blank=True)
     def __str__(self):
         return self.account.username
     class Meta:
